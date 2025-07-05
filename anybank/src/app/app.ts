79f98e41c690204ec301a36d06dfb21592f724ec
walacefex <walacefex01@gmail.com>
2025-07-04 22:21:01 -0300

    feat: creating banner, conta and boas-vindas components

--- anybank/src/app/app.ts
+++ anybank/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from './components/banner/banner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [BannerComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'anybank';
